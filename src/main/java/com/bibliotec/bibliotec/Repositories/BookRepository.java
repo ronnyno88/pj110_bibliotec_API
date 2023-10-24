774626d0dedb6c2123ebb3f8b0ddfe83d556eba6
ronnyno88 <95188001+ronnyno88@users.noreply.github.com>
2023-10-24 14:13:47 -0300

    transforming repositories in interfaces and extending JpaRepository

--- src/main/java/com/bibliotec/bibliotec/Repositories/BookRepository.java
+++ src/main/java/com/bibliotec/bibliotec/Repositories/BookRepository.java
@@ -1,7 +1,8 @@
 package com.bibliotec.bibliotec.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BookRepository {
+public interface BookRepository extends JpaRepository {
 }
--- src/main/java/com/bibliotec/bibliotec/Repositories/CategoryRepository.java
+++ src/main/java/com/bibliotec/bibliotec/Repositories/CategoryRepository.java
@@ -1,7 +1,8 @@
 package com.bibliotec.bibliotec.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CategoryRepository {
+public interface CategoryRepository extends JpaRepository {
 }
